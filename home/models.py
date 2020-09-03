ba30be286c7c3c4ac6680fe59608f9959f7fd0f2
Crowdbotics <team@crowdbotics.com>
2020-09-03 14:23:39 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,20 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +28,32 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class Podcaster(models.Model):
+    "Generated Model"
+    name = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="podcaster_name",
+    )
+    podcast_name = models.CharField(
+        max_length=256,
+        null=True,
+        blank=True,
+    )
+    podcast_description = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    podcast_link = models.TextField(
+        null=True,
+        blank=True,
+    )
